8d7e655f62a41328e04dd34e7e7c1d3e042c78c7
Shenghao <shenghaodong18@gmail.com>
2022-12-09 11:07:34 -0500

    added main and funcs files

---
+++ funcs.cpp
@@ -0,0 +1,23 @@
+//Task E is required
+//Task A should return a string\
+
+//Task A
+std::string printRange(int left, int right){
+    std::string returnVal;
+    if(left < right){
+        
+    }else{
+        return returnVal;
+    }
+
+}
+
+//Task B
+
+//Task C
+
+//Task D
+
+//Task E
+
+//Task F
