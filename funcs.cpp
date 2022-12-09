4f9ded1740d4b9b3b3ce0df6bf3dced4d5f6187b
Shenghao <shenghaodong18@gmail.com>
2022-12-09 11:28:24 -0500

    upd for work at home

--- funcs.cpp
+++ funcs.cpp
@@ -1,14 +1,17 @@
 //Task E is required
-//Task A should return a string\
+//Task A should return a string
+#include <string>
 
 //Task A
 std::string printRange(int left, int right){
     std::string returnVal;
     if(left < right){
-        
+        returnVal += std::string(right);
+        printrange(left, right - 1);
     }else{
         return returnVal;
     }
+    return returnVal;
 
 }
 
