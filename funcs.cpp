6a598a81d95f5d56a7105b7bba93ed9c1f31e506
Shenghao Dong <shenghaodong18@gmail.com>
2022-12-11 00:44:42 -0500

    done

--- funcs.cpp
+++ funcs.cpp
@@ -5,17 +5,32 @@
 
 //Task A
 std::string printRange(int left, int right){
-    static std::string returnVal;
+    static std::string dummyVal;
+    std::string returnVal;
         if(left == right + 1){
+            returnVal = dummyVal;
+            dummyVal = "";
             return returnVal;
         }else{
-            returnVal += std::to_string(left) + " ";
+            dummyVal += std::to_string(left) + " ";
             return printRange(left + 1, right);
         }
 
 }
 
 //Task B
+int sumRange(int left, int right){
+    static int dummyVal;
+    int returnVal;
+    if(left == right + 1){
+        returnVal = dummyVal;
+        dummyVal = 0;
+        return returnVal;
+    }else{
+        dummyVal += left;
+        return sumRange(left + 1, right);
+    }
+}
 
 //Task C
 
--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 
 int main(){
     std::cout << printRange(-3, 6) << std::endl;
+    std::cout << sumRange(-2, 10) << std::endl;
     return 0;
 }
