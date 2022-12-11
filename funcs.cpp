f90c8f47ed338f248f2a7f40d083e0f66904a219
Shenghao Dong <shenghaodong18@gmail.com>
2022-12-11 00:36:34 -0500

    finsished with task a

--- funcs.cpp
+++ funcs.cpp
@@ -1,17 +1,17 @@
 //Task E is required
 //Task A should return a string
 #include <string>
+#include <iostream>
 
 //Task A
 std::string printRange(int left, int right){
-    std::string returnVal = "";
-    if (right > left) {
-        std::cout << "test" << std::endl;
-        returnVal += std::to_string(left) + " ";
-        printRange(left + 1, right);
-    }else {
-        return returnVal;
-    }
+    static std::string returnVal;
+        if(left == right + 1){
+            return returnVal;
+        }else{
+            returnVal += std::to_string(left) + " ";
+            return printRange(left + 1, right);
+        }
 
 }
 
--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 #include "funcs.h"
 
 int main(){
-    std::cout << printRange(0, 39) << std::endl;
+    std::cout << printRange(-3, 6) << std::endl;
     return 0;
 }
